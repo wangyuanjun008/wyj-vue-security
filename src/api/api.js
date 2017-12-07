92da81a7093a741b2c7fd27ba626f91f5162decc
wangyuanjun008 <1098345452@qq.com>
2017-12-07 23:27:32 +0800

    增加数据字典方法

--- src/api/api.js
+++ src/api/api.js
@@ -5,25 +5,44 @@ let base = '';
 
 export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
 
-export const getUserList = params => { return axios.get(`${base}/user/list`, { params: params }); };
+//用户管理
+export const getUserListPage = params => { return axios.post(`/remote/user/list`, qs.stringify(params)); };
 
-export const getUserListPage = params => { return axios.get(`${base}/user/listpage`, { params: params }); };
+export const removeUser = params => { return axios.post(`/remote/user/remove`, qs.stringify(params)); };
 
-export const removeUser = params => { return axios.get(`${base}/user/remove`, { params: params }); };
+export const editUser = params => { return axios.get(`/remote/user/`+params); };
 
-export const batchRemoveUser = params => { return axios.get(`${base}/user/batchremove`, { params: params }); };
+export const addUser = params => { return axios.post(`/remote/user/add`, qs.stringify(params)); };
 
-export const editUser = params => { return axios.get(`${base}/user/edit`, { params: params }); };
-
-export const addUser = params => { return axios.get(`${base}/user/add`, { params: params }); };
-
-//export const getDataGroupListPage = params => { return axios.get(`http://localhost:8081/remote/dataGroup/list`, { params: params }); };
-
+//数据分组
 export const getDataGroupListPage = params => { return axios.post(`/remote/dataGroup/list`, qs.stringify(params)); };
 
-export const addDataGroup = params => { return axios.post(`/remote/dataGroup/add`, qs.stringify(params) ); };
+export const addDataGroup = params => { return axios.post(`/remote/dataGroup/add`, params); };
 
 export const removeDataGroup = params => { return axios.post(`/remote/dataGroup/remove`, qs.stringify(params)); };
 
-//export const getData = params => {  axios.get(`/remote/dataDict/getData`, { params }).then((res)=>{return res.data}); };
+
+//数据字典
+export const getDataDictListPage = params => { return axios.post(`/remote/dataDict/list`, qs.stringify(params)); };
+
+export const addDataDict = params => { return axios.post(`/remote/dataDict/add`, qs.stringify(params) ); };
+
+export const removeDataDict = params => { return axios.post(`/remote/dataDict/remove`, qs.stringify(params)); };
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//获取下拉框数据源 
 export const getData = params => {return  axios.get(`/remote/dataDict/getData`, { params }); };
+
+export const getDataStore = url => {    
+	var dataStore;
+    $.ajax({
+        dataType : 'json',
+        type : 'get',
+        url : url,
+        async : false,
+        success : function(data) {
+            dataStore = data;
+        }
+    });
+    return dataStore;
+};
