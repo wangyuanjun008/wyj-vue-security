7a5f4494e9963af9be68ae363086081fe280f566
wangyuanjun008 <1098345452@qq.com>
2017-12-14 11:07:16 +0800

    增加首页页面homePage.vue

---
+++ src/view/homePage/homePage.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<h3>&nbsp;&nbsp;&nbsp;项目介绍</h3>
+		<ul>
+			<li>一个轻量级的Java快速开发平台(框架)，能快速开发项目并交付</li>
+			<li>友好的代码结构及注释，便于阅读及二次开发</li>
+			<li>命名规范和工程分层规约参考阿里巴巴JAVA开发规范</li>
+			<li>基于角色的权限管理，细分到按钮权限</li>
+			<li>单点登录(SSO)，提供统一用户单点登录认证、用户鉴权功能</li>
+			<li>用户会话管理，提供分布式用户会话管理</li>
+			<li>操作日志管理，提供记录用户登录、操作等日志</li>
+			<li>基于Maven模块化开发，可快速实现个性化业务模块</li>
+		</ul>
+		<h3>&nbsp;&nbsp;&nbsp;获取帮助</h3>
+		<ul>
+			<li>git地址：<a href="https://github.com/wangyuanjun008/wyj-parent.git" target="_blank">https://github.com/wangyuanjun008/wyj-parent.git</a></li>
+			<li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
+		</ul>	
+	</div>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+			}
+		},
+		methods: {
+		},
+		mounted() {
+		}
+	}
+</script>
