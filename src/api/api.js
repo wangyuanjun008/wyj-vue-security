507bb7340fc025ebc5a4ef08032c60c3a576dbf9
wangyuanjun008 <1098345452@qq.com>
2017-12-14 11:16:36 +0800

    增加axios方法

--- src/api/api.js
+++ src/api/api.js
@@ -3,7 +3,8 @@ import qs from 'qs';
 
 let base = '';
 
-export const requestLogin = params => { return axios.post(`${base}/login`, params).then(res => res.data); };
+//登录
+export const requestLogin = params => { return axios.post(`/remote/login`, qs.stringify(params)).then(res => res.data); };
 
 //用户管理
 export const getUserListPage = params => { return axios.post(`/remote/user/list`, qs.stringify(params)); };
@@ -13,6 +14,27 @@ export const removeUser = params => { return axios.post(`/remote/user/remove`, q
 export const editUser = params => { return axios.get(`/remote/user/`+params); };
 
 export const addUser = params => { return axios.post(`/remote/user/add`, qs.stringify(params)); };
+
+//角色管理
+export const getRoleListPage = params => { return axios.post(`/remote/role/list`, qs.stringify(params)); };
+
+export const addRole= params => { return axios.post(`/remote/role/add`, qs.stringify(params)); };
+
+export const removeRole = params => { return axios.post(`/remote/role/remove`, qs.stringify(params)); };
+
+//菜单管理
+export const getMenuListPage = params => { return axios.post(`/remote/menu/list`, qs.stringify(params)); };
+
+export const addMenu = params => { return axios.post(`/remote/menu/add`, qs.stringify(params)); };
+
+export const removeMenu = params => { return axios.post(`/remote/menu/remove`, qs.stringify(params)); };
+
+//权限管理
+export const getAuthListPage = params => { return axios.post(`/remote/auth/list`, qs.stringify(params)); };
+
+export const addAuth = params => { return axios.post(`/remote/auth/add`, qs.stringify(params)); };
+
+export const removeAuth = params => { return axios.post(`/remote/auth/remove`, qs.stringify(params)); };
 
 //数据分组
 export const getDataGroupListPage = params => { return axios.post(`/remote/dataGroup/list`, qs.stringify(params)); };
@@ -28,6 +50,11 @@ export const getDataDictListPage = params => { return axios.post(`/remote/dataDi
 export const addDataDict = params => { return axios.post(`/remote/dataDict/add`, qs.stringify(params) ); };
 
 export const removeDataDict = params => { return axios.post(`/remote/dataDict/remove`, qs.stringify(params)); };
+
+//日志
+export const getLogPage = params => { return axios.post(`/remote/log/list`, qs.stringify(params)); };
+
+export const removeLog = params => { return axios.post(`/remote/log/remove`, qs.stringify(params)); };
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //获取下拉框数据源 
